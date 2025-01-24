f7bcc077a5224b2e9db6a09912e68a3d33ad387b
leVert <selmamathis15@gmail.com>
2025-01-24 16:17:06 +0300

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 3000, // Set the server port to 3000
+		host: '0.0.0.0', // Allow access from outside the container
+	  },
 });
